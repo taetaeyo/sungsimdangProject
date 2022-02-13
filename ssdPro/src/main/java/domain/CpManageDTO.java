471beb44cd5de0e125a5e37f42c8b7040d81b2b4
taekwon <op_o@naver.com>
2022-02-14 00:37:53 +0900

    JSP Pro commit

---
+++ ssdPro/src/main/java/domain/CpManageDTO.java
@@ -0,0 +1,46 @@
+package domain;
+
+import java.util.Date;
+
+public class CpManageDTO {
+
+	private String coupon_code;			// �����ڵ�
+	private String cp_name;		// �����ؿܿ���
+	private String manager_code;		// �����ڹ�ȣ
+	private Date create_date;		// ����
+	
+	public String getCoupon_code() {
+		return coupon_code;
+	}
+	public void setCoupon_code(String coupon_code) {
+		this.coupon_code = coupon_code;
+	}
+	public String getCp_name() {
+		return cp_name;
+	}
+	public void setCp_name(String cp_name) {
+		this.cp_name = cp_name;
+	}
+	public String getManager_code() {
+		return manager_code;
+	}
+	public void setManager_code(String manager_code) {
+		this.manager_code = manager_code;
+	}
+	public Date getCreate_date() {
+		return create_date;
+	}
+	public void setCreate_date(Date create_date) {
+		this.create_date = create_date;
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
---
+++ ssdPro/src/main/java/domain/StoManageDTO.java
@@ -0,0 +1,92 @@
+package domain;
+
+
+
+
+public class StoManageDTO {
+
+	private String store_code;			// �����ڵ�
+	private String manager_code;		// �����ڹ�ȣ
+	private Integer is_global;		// �����ؿܿ���
+	private String region;		// ����
+	private String name;		// �����
+	private String brand;		// �귣��
+	private String tel;		// ��ȭ��ȣ
+	private String address;		// �ּ�
+	private String operating_time;		// ��ð�
+	private String lat;		// ����
+	private String lng;		// �浵
+	
+	public String getStore_code() {
+		return store_code;
+	}
+	public void setStore_code(String store_code) {
+		this.store_code = store_code;
+	}
+	public String getManager_code() {
+		return manager_code;
+	}
+	public void setManager_code(String manager_code) {
+		this.manager_code = manager_code;
+	}
+	public Integer getIs_global() {
+		return is_global;
+	}
+	public void setIs_global(Integer is_global) {
+		this.is_global = is_global;
+	}
+	public String getRegion() {
+		return region;
+	}
+	public void setRegion(String region) {
+		this.region = region;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getOperating_time() {
+		return operating_time;
+	}
+	public void setOperating_time(String operating_time) {
+		this.operating_time = operating_time;
+	}
+	public String getLat() {
+		return lat;
+	}
+	public void setLat(String lat) {
+		this.lat = lat;
+	}
+	public String getLng() {
+		return lng;
+	}
+	public void setLng(String lng) {
+		this.lng = lng;
+	}
+	
+	
+	
+	
+	
+
+}
